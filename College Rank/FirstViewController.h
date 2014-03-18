58715c5f2a261ac45189980107c7757128e3a655
pbontrager <pbontrager@gmail.com>
2014-03-18 12:04:36 -0400

    First Attempt at enabling search

--- College Rank/FirstViewController.h
+++ College Rank/FirstViewController.h
@@ -2,7 +2,7 @@
 //  FirstViewController.h
 //  College Rank
 //
-//  Created by Jacob Levi Shetler on 2/13/14.
+//  Created by Philip Bontrager on 3/18/14.
 //  Copyright (c) 2014 GCInformatics. All rights reserved.
 //
 
--- College Rank/FirstViewController.m
+++ College Rank/FirstViewController.m
@@ -2,23 +2,42 @@
 //  FirstViewController.m
 //  College Rank
 //
-//  Created by Jacob Levi Shetler on 2/13/14.
+//  Created by Philip Bontrager on 3/18/14.
 //  Copyright (c) 2014 GCInformatics. All rights reserved.
 //
 
+#import "InstitutionManager.h"
+#import "Institution.h"
 #import "FirstViewController.h"
-#import "DataRetreiver.h"
-#import "Calculations.h"
+
 @interface FirstViewController ()
+
+@property InstitutionManager* institutions;
+@property NSArray* searchResults;
 
 @end
 
 @implementation FirstViewController
 
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+
+    }
+    return self;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    _institutions = [InstitutionManager sharedInstance];
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,5 +45,109 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Search Results
+
+- (void)filterContentForSearchText:(NSString*)searchText scope:(NSString*)scope
+{
+#warning allInstitutions is a list of strings, need zip code
+    NSPredicate *resultPredicate = [NSPredicate predicateWithFormat:@"name contains[c] %@", searchText];
+    _searchResults = [[_institutions allInstitutions] filteredArrayUsingPredicate:resultPredicate];
+}
+
+-(BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
+{
+    [self filterContentForSearchText:searchString
+                               scope:[[self.searchDisplayController.searchBar scopeButtonTitles]
+                                      objectAtIndex:[self.searchDisplayController.searchBar
+                                                     selectedScopeButtonIndex]]];
+    
+    return YES;
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        return [_searchResults count];
+        
+    } else {
+        return [[_institutions userInstitutions] count];
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    Institution* inst = nil;
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        inst = [_searchResults objectAtIndex:indexPath.row];
+    } else {
+        inst = [[_institutions userInstitutions] objectAtIndex:indexPath.row];
+    }
+    cell.textLabel.text = inst.name;
+    cell.detailTextLabel.text = inst.location;
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+ */
 
 @end
